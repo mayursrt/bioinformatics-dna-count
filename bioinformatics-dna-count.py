21886ac9fa9875c76f80619978d7ff7d1763d427
mayursrt <mayurmachhi19@gmail.com>
2021-04-29 04:16:19 +0530

    initial commit

---
+++ bioinformatics-dna-count.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import streamlit as st
+import altair as alt
+
+## Title
+
+st.write("""
+
+# DNA Nucleotide Counter
+
+This app counts the Nucleotide composition of the query DNA
+	""")
+
+## Input Box
+
