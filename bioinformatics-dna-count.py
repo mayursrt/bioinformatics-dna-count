ca075a048d3971d9410b7612e061638d821f4e5f
mayursrt <mayurmachhi19@gmail.com>
2021-04-29 23:44:40 +0530

    app

--- bioinformatics-dna-count.py
+++ bioinformatics-dna-count.py
@@ -3,6 +3,7 @@ import streamlit as st
 import altair as alt
 from PIL import Image
 
+st.set_page_config(layout="centered")
 ## Title
 
 title_container = st.beta_container()
@@ -14,12 +15,11 @@ with title_container:
     with col2:
         st.write("""
         	# DNA Nucleotide Counter App\n
-        	This app counts the Nucleotide composition of the query DNA
-
+This app counts the Nucleotide composition of the query DNA\n
         	""")
     with col3:
         st.image(image)
-
+st.write('-by Mayur Machhi')
 
 ## Input Box
 
@@ -31,3 +31,60 @@ sequence = st.text_area("Sequence input", sequence_input, height=250)
 sequence = sequence.splitlines()
 sequence = sequence[1:] # Skips the sequence name (first line)
 sequence = ''.join(sequence) # Concatenates list to string
+
+st.write("""
+***
+	""")
+
+st.header('Input DNA Query')
+
+
+placeholder = st.empty()
+placeholder.text(sequence)
+
+st.header('Output DNA Nucleotide Count')
+
+## 1. Print dictionary
+# st.subheader('1. Print dictionary')
+def DNA_nucleotide_count(seq):
+  d = dict([
+            ('A',seq.count('A')),
+            ('T',seq.count('T')),
+            ('G',seq.count('G')),
+            ('C',seq.count('C'))
+            ])
+  return d
+
+X = DNA_nucleotide_count(sequence)
+
+
+### 1. Print text
+st.subheader('1. Print text')
+st.write('There are  ' + str(X['A']) + ' adenine (A)')
+st.write('There are  ' + str(X['T']) + ' thymine (T)')
+st.write('There are  ' + str(X['G']) + ' guanine (G)')
+st.write('There are  ' + str(X['C']) + ' cytosine (C)')
+
+### 3. Display DataFrame
+# st.subheader('3. Display DataFrame')
+df = pd.DataFrame.from_dict(X, orient='index')
+df = df.rename({0: 'count'}, axis='columns')
+df.reset_index(inplace=True)
+df = df.rename(columns = {'index':'nucleotide'})
+# st.write(df)
+
+### 4. Display Bar Chart using Altair
+st.subheader('4. Display Bar chart')
+p = alt.Chart(df).mark_bar().encode(
+    x='nucleotide',
+    y='count'
+)
+p = p.properties(
+    width=alt.Step(80)  # controls width of bar.
+)
+st.write(p)
+
+
+
+
+st.write("-App by Mayur Machhi")
